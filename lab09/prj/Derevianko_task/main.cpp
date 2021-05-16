096c4c72ded4612fe8bb511be21d361d2bded3d9
LichHunter <alexander0derevianko@gmail.com>
2021-05-16 08:50:09 +0100

    Changed localisation

--- lab09/prj/Derevianko_task/main.cpp
+++ lab09/prj/Derevianko_task/main.cpp
@@ -10,7 +10,7 @@
 using namespace std;
 
 void localisation() {
-    _wsetlocale(LC_ALL,L" ");
+    _wsetlocale(LC_ALL,L"uk_UA.UTF-8");
     _setmode(_fileno(stdout), _O_U8TEXT);
 }
 
@@ -19,11 +19,17 @@ void function81() {
     double y;
     double z;
 
-    wcout << L"Уведіть x: ";
+    wcout << L"Уведiть x: ";
     cin >> x;
-    wcout << L"Уведіть y: ";
-    cin >> y;
-    wcout << L"Уведіть z: ";
+    do {
+        wcout << L"Уведiть y: ";
+        cin >> y;
+
+        if (y == 0) {
+            wcout << L"Y повинно бути бiльше 0" << endl;
+        }
+    } while (y == 0);
+    wcout << L"Уведiть z: ";
     cin >> z;
 
     wcout << L"S = " << s_calculate(x, y, z) << endl;
@@ -31,7 +37,7 @@ void function81() {
 
 void function91() {
     int input = 0;
-    wcout << L"Уведіть об'єм використиного газу за місяць: ";
+    wcout << L"Уведiть об'єм використиного газу за мiсяць: ";
     cin >> input;
 
     wcout << L"Сума до сплати - " << sumToPay(input) << endl;
@@ -39,36 +45,36 @@ void function91() {
 
 void function92() {
     double input = 0;
-    wcout << L"Уведіть розмір взуття у сантиметрах: ";
+    wcout << L"Уведiть розмiр взуття у сантиметрах: ";
     cin >> input;
 
     double ukranian = centemetrToUkranianSize(input);
     double british = centemetrToBritishSize(input);
 
     if (ukranian == 0.0) {
-        wcout << L"Розмір взуття в системі України – " << L"не існує" << endl;
+        wcout << L"Розмiр взуття в системi України – " << L"не iснує" << endl;
     } else {
-        wcout << L"Розмір взуття в системі України – " << ukranian << endl;
+        wcout << L"Розмiр взуття в системi України – " << ukranian << endl;
     }
 
-    wcout << L"Розмір взуття в системі Великобританії - " << british << endl;
+    wcout << L"Розмiр взуття в системi Великобританiї - " << british << endl;
 }
 
 void function93() {
     int input = -1;
 
     do {
-        wcout << L"Уведіть число: " << endl;
+        wcout << L"Уведiть число: " << endl;
         cin >> input;
         if (input >= 0) {
             int d11 = 2048;
             if (input & d11) {
-                wcout << L"Число двійкових нулів - " << countNumber(input) << endl;
+                wcout << L"Число двiйкових нулiв - " << countNumber(input) << endl;
             } else {
-                wcout << L"Число двійкових одиниць - " << countNumber(input) << endl;
+                wcout << L"Число двiйкових одиниць - " << countNumber(input) << endl;
             }
         } else {
-            wcout << L"Число повинно бути більше 0" << endl;
+            wcout << L"Число повинно бути бiльше 0" << endl;
         }
     } while (input < 0);
 
@@ -82,33 +88,33 @@ int main()
     char input = 0;
 
     do {
-        wcout << L"Уведіть \"u\" – для виклику функції s_calculate, \"t\" – для виклику функціїї до задачі 9.1, " << endl;
-        wcout << L"\"r\" – для виклику функції до задачі 9.2 або \"e\" – для виклику функції до задачі 9.3:" << endl;
+        wcout << L"Уведiть \"u\" – для виклику функцiї s_calculate, \"t\" – для виклику функцiїї до задачi 9.1, " << endl;
+        wcout << L"\"r\" – для виклику функцiї до задачi 9.2 або \"e\" – для виклику функцiї до задачi 9.3:" << endl;
         cin >> input;
 
         if (input == 'u') {
             function81();
 
-            wcout << L"Для виходу уведіть \"w\"  або \"i\":" << endl;
+            wcout << L"Для виходу уведiть \"w\"  або \"i\":" << endl;
             cin >> input;
         } else if (input == 't') {
             function91();
 
-            wcout << L"Для виходу уведіть \"w\"  або \"i\":" << endl;
+            wcout << L"Для виходу уведiть \"w\"  або \"i\":" << endl;
             cin >> input;
         } else if (input == 'r') {
             function92();
 
-            wcout << L"Для виходу уведіть \"w\"  або \"i\":" << endl;
+            wcout << L"Для виходу уведiть \"w\"  або \"i\":" << endl;
             cin >> input;
         } else if (input == 'e') {
             function93();
 
-            wcout << L"Для виходу уведіть \"w\"  або \"i\":" << endl;
+            wcout << L"Для виходу уведiть \"w\"  або \"i\":" << endl;
             cin >> input;
         } else if (input != 'w' && input != 'i') {
             Beep(523, 500);
-            wcout << L"Не вірний ввод" << endl;
+            wcout << L"Не вiрний ввод" << endl;
         }
 
     } while (input != 'w' && input != 'i');
